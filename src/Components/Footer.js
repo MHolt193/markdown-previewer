9b10d5186668717ea92204dbc4692be3c1a1a63e
MHolt193 <76035004+MHolt193@users.noreply.github.com>
2021-07-25 14:32:59 -0600

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,7 +2,7 @@ import './Components.css'
 const Footer = () => {
     return (
         <div className="text-center">
-            <p>Coded by Michael Holt, as a freeCodeCamp project.  <a href="https://github.com/MHolt193/Random-Quote-Machine/tree/main/random-quote-machine" target="_blank" rel="noreferrer">Source Code:<i className="fab fa-github-square"></i></a></p>
+            <p>Coded by Michael Holt, as a freeCodeCamp project.  <a href="https://github.com/MHolt193/markdown-previewer" target="_blank" rel="noreferrer">Source Code:<i className="fab fa-github-square"></i></a></p>
             
         </div>
     )
